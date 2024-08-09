30b3d3fd54e42d64a7ca022a904d61cd5be2b2c1
Santiago Mesa <thiagomesa21@gmail.com>
2024-08-08 20:47:38 -0500

    app de prueba
    en este commit se sube todo el live coding.

---
+++ tienda/productos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Cliente
+from .models import Producto
+
+# Create your views here.
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'listar_clientes.html', {'clientes': clientes})
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar_productos.html', {'productos': productos})
+
