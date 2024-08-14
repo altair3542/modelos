d575cdc6f214ff376edc49919983320f5dcbad06
Santiago Mesa <thiagomesa21@gmail.com>
2024-08-13 22:50:14 -0500

    Cambios sesion 3
    En este commit se hacen cambios para agregar los formularios tanto para cliente como para producto
    
    Se agregan las plantillas html que renderizan esso formularios y se coloca una validacion a nivel de formulario para que muestre los errores. ademas de una validacion a nivel de vista.
    
    se recomienda confirmar que sus bases de codigo sean iguales a esta antes de continuar con las sesiones.

--- tienda/productos/views.py
+++ tienda/productos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cliente
 from .models import Producto
+from .forms import ProductoModelForm, ClienteModelForm
 
 # Create your views here.
 
@@ -14,3 +15,24 @@ def listar_productos(request):
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoModelForm
+    return render(request, 'agregar_producto.html', {'form': form})
+
+
+def agregar_cliente(request):
+    if request.method == 'POST':
+        form = ClienteModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteModelForm()
+    return render(request, 'agregar_cliente.html', { 'form': form })
