eb16f1c86e9d9755845036415de9d8feed85925e
Santiago Mesa <thiagomesa21@gmail.com>
2024-08-15 20:43:56 -0500

    Cuarta sesion
    En esta sesion, agregamos un filtro de precio, un firltro de busqueda y un paginador.

--- tienda/productos/forms.py
+++ tienda/productos/forms.py
@@ -10,8 +10,14 @@ class ProductoModelForm(forms.ModelForm):
     def clean_precio(self):
         precio = self.cleaned_data.get('precio')
         if precio <= 0: #con esta validacion nos aseguramos que aparezca un error si el precio es menor a cero.
-            raise forms.ValidationError('El precio debe ser mayor que cero. ')
+            raise forms.ValidationError('usted que piensa de la vida colocando precios en negativo? ')
         return precio
+
+    def clean_stock(self):
+        stock = self.cleaned_data.get('stock')
+        if stock <= 0:
+            raise forms.ValidationError('Como vas a vender lo que no tienes?')
+        return stock
 
 class ClienteModelForm(forms.ModelForm):
     class Meta:
--- tienda/productos/views.py
+++ tienda/productos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Cliente
 from .models import Producto
 from .forms import ProductoModelForm, ClienteModelForm
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -10,7 +12,22 @@ def listar_clientes(request):
     return render(request, 'listar_clientes.html', {'clientes': clientes})
 
 def listar_productos(request):
-    productos = Producto.objects.all()
+    query = request.GET.get('q')
+    productos_list = Producto.objects.all()
+    paginator = Paginator(productos_list, 10)
+    precio_min = request.GET.get('precio_min')
+    precio_max = request.GET.get('precio_max')
+
+    page_number = request.GET.get('page')
+    productos = paginator.get_page(page_number)
+
+    if precio_min and precio_max:
+        productos_list = productos_list.filter(precio__gte=precio_min, precio__lte=precio_max)
+
+    if query:
+        productos_list = productos_list.filter(Q(nombre__icontains=query)| Q(descripcion__icontains=query))
+
+
     return render(request, 'listar_productos.html', {'productos': productos})
 
 def inicio(request):
